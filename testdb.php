9f029bc0bef82fbda30b6a83e511f71f8fab0ef5
saiedghada393-oss <saiedghada393@gmail.com>
2026-05-08 20:52:29 +0100

    Update testdb.php

--- testdb.php
+++ testdb.php
@@ -1,11 +1,17 @@
 <?php
 
 $host = "sql107.infinityfree.com";
-$db   = "if0_41856900_environet";
+$db   = "if0_41856900_environet_db";
 $user = "if0_41856900";
 $pass = "9QpLpcTgRAH";
 
+  $pdo = new PDO(
+    "mysql:host=sql107.infinityfree.com;port=3306;dbname=if0_41856900_environet_db;charset=utf8",
+    "if0_41856900",
+    "9QpLpcTgRAH"
+);
 try {
+  
 
     $pdo = new PDO(
         "mysql:host=$host;dbname=$db;charset=utf8",
